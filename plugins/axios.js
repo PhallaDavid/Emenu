9ee976e711d2f50fe0cfe1ad7b791da6b80f164b
Phalladavid1 <david.phalla@codexe.dev>
2025-10-01 16:21:54 +0700

    booking

--- plugins/axios.js
+++ plugins/axios.js
@@ -2,7 +2,7 @@
 
   export default defineNuxtPlugin((nuxtApp) => {
     const api = axios.create({
-      baseURL: "http://54.196.237.240/api",
+      baseURL: "https://54.196.237.240/api",
     });
     nuxtApp.provide("api", api);
   });
